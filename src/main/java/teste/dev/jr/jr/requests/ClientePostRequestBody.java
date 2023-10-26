12f9afb21818d5c357fa1d089f14ff93d65f58f4
igorfelip <igorfelipe844@gmail.com>
2023-10-26 01:17:55 -0300

    feat: create Cliente and Empresa request Body´s

---
+++ src/main/java/teste/dev/jr/jr/requests/ClientePostRequestBody.java
@@ -0,0 +1,16 @@
+package teste.dev.jr.jr.requests;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Builder
+public class ClientePostRequestBody {
+    @NotEmpty(message = "Insira o nome")
+    private String nome;
+    @NotEmpty(message = "Insira o CPF")
+    private Long cpf;
+    private Double saldo;
+}
