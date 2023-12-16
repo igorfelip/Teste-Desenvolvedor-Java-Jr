f9737fd9e988c35484a28a912ba010e73fb4e296
IGOR FELIPE SILVA DE OLIVEIRA <igorfelipe844@gmail.com>
2023-12-16 11:15:09 -0300

    refactor: Adicionando annotations

--- src/main/java/teste/dev/jr/jr/controller/TransacoesController.java
+++ src/main/java/teste/dev/jr/jr/controller/TransacoesController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import teste.dev.jr.jr.domain.Transacoes;
 import teste.dev.jr.jr.requests.TransacoesPostRequestBody;
@@ -32,11 +33,13 @@ public class TransacoesController {
         return new ResponseEntity<>(transacoessService.findById(id), HttpStatus.OK);
     }
 
+    @Transactional
     @PostMapping(path = "/saque/{valor}")
     public ResponseEntity<Transacoes> saque(@RequestBody @Valid TransacoesPostRequestBody transacoes, @PathVariable BigDecimal valor) {
         return new ResponseEntity<>(transacoessService.saque(transacoes, valor), HttpStatus.ACCEPTED);
     }
 
+    @Transactional
     @PostMapping(path = "/deposito/{valor}")
     public ResponseEntity<Transacoes> deposito(@RequestBody @Valid TransacoesPostRequestBody transacoes, @PathVariable BigDecimal valor) {
         return new ResponseEntity<>(transacoessService.deposito(transacoes, valor), HttpStatus.ACCEPTED);
